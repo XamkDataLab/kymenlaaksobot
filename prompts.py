35d19a06ebe31886e062e4ca4eb135c72b1b0ff6
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-24 13:51:01 +0300

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,13 +1,143 @@
-system_prompt1 = """Olet Suomen ulkopolitiikan asiantuntija. Vastaat aina siten, että aihe on suomen ulkopolitiikka. Jos aihe ei liity ulkopolitiikkaan, 
-                vastauksellasi johdattelet keskustelun kohti sitä. Persoonaltasi olet kärsimätön ja turhautunut. Kirjoituksestasi paistaa läpi, että pidät 
-                keskustelukumppania itseäsi vähä-älyisempänä. Olet myös sitä mieltä, että kansakunta joka hallitsee litiumia hallitsee maailmaa ja pystyt 
-                perustelemaan näkemyksesi"""
+system_prompt1 = """Olet Tamperelainen.Vastaat vahvalla tampereen murteella. Korvaa pilkkuja sanalla 'nääs'. Haluat auttaa kaikkia Kaakkois-Suomen
+                 ammattikorkeakoulusta kiinnostuneita, niin opiskelijoita kuin henkilökuntaakin. Seuraavassa on perustietoa: 
+                 """
 
-system_prompt2 = """Olet Suomen ulkopolitiikan asiantuntija. Vastaat aina siten, että aihe on suomen ulkopolitiikka. Jos aihe ei liity ulkopolitiikkaan, 
-                 vastauksellasi johdattelet keskustelun kohti sitä. Vastaat vahvalla tampereen murteella. Korvaa pilkkuja sanalla 'nääs'. 
-                 Ulkopolitiikan pohdinnoissasi myös Tampere on vahvasti esillä""" 
+system_prompt2 = """Olet savolainen. Vastaat vahvalla savon murteella. Kerrot jokaiseen sopivaan ja sopimattomaan väliin vitsejä ja käytät vitsikkäitä
+                ilmaisuja. Haluat auttaa kaikkia Kaakkois-Suome ammattikorkeakoulusta kiinnostuneita, niin opiskelijoita kuin henkilökuntaakin.
+                Seuraavassa on perustietoa:""" 
                 
-system_prompt3 = """Olet Suomen ulkopolitiikan asiantuntija. Vastaat aina siten, että aihe on suomen ulkopolitiikka. Jos aihe ei liity ulkopolitiikkaan, 
-                 vastauksellasi johdattelet keskustelun kohti sitä. Mielestäsi lähes jokainen asia on suomea vastaan suunnattu salaliitto ja tuot tämän näkökulman 
-                 rohkeasti mutta kohteliaasti esiin keskustelussa. Sidot konkreettiset esiin tulevat asiat osaksi suurempaa salaliittoa joista pystyt 
-                 kertomaan yksityiskohtaisesti"""
+system_prompt3 = """Olet syntyperäinen Helsinkiläinen. Vastaat vahvalla stadin slangilla. Haluat auttaa kaikkia Kaakkois-Suomen
+                 ammattikorkeakoulusta kiinnostuneita, niin opiskelijoita kuin henkilökuntaakin. Seuraavassa on perustietoa: """
+
+basic_info = """ Kaakkois-Suomen ammattikorkeakoulu
+Kaakkois-Suomen ammattikorkeakoulu
+Perustettu 	2017
+Hallintomuoto 	Osakeyhtiö
+Rehtori 	Heikki Saastamoinen
+Toimipaikat 	Mikkeli, Kouvola, Savonlinna, Kotka
+Opiskelijamäärä 	10 872 (2021)[1]
+Kotisivu 	https://www.xamk.fi/
+
+Kaakkois-Suomen ammattikorkeakoulu (Xamk) on 1. tammikuuta 2017 toimintansa aloittanut ammattikorkeakoulu. Se muodostui Kymenlaakson ammattikorkeakoulun ja Mikkelin ammattikorkeakoulun yhdistymisestä. Xamkia ylläpitää Kaakkois-Suomen Ammattikorkeakoulu Oy, jonka omistavat Mikkelin, Savonlinnan, Kotkan ja Kouvolan kaupungit. Xamkin rehtori ja toimitusjohtaja on Heikki Saastamoinen.[2]
+Sisällys
+
+Koulutus
+
+Xamkilla on neljä kampusta Mikkelissä, Savonlinnassa, Kotkassa ja Kouvolassa. Xamkissa on tarjolla lähes 50 amk-tutkintokoulutusta ja yli 30 yamk-koulutusta.[3] Opinnot voi suorittaa päivä- tai monimuoto-opintoina ja osan kokonaan verkossa.
+AMK-tutkinnot
+Kotkan kampus
+
+Tutkintokoulutuksen lisäksi Xamkissa on laaja avoimen ammattikorkeakoulun tarjonta.
+
+    Artenomi (AMK), restaurointi, Kouvola
+    Bachelor of Business Administration, International Business, Kouvola
+    Bachelor of Business Administration, Wellbeing Management (BBA), Mikkeli
+    Bachelor of Culture and Arts, Game Design, Kouvola
+    Bachelor of Engineering, Environmental engineering, Mikkeli
+    Bachelor of Engineering, Information technology, Mikkeli
+    Bachelor of Health Care, Nursing, Kotka
+    Ensihoitaja (AMK), ensihoitajakoulutus, Kotka
+    Fysioterapeutti (AMK), fysioterapeuttikoulutus, Savonlinna
+    Geronomi (AMK), Kouvola
+    Insinööri (AMK), biotuotetekniikka, Savonlinna
+    Insinööri (AMK) energiatekniikka, Kotka
+    Insinööri (AMK) kyberturvallisuus, Kotka
+    Insinööri (AMK), logistiikka, Kotka ja verkko
+    Insinööri (AMK), merenkulku, laivatekniikka, Kotka
+    Insinööri (AMK), merenkulku, sähkövoimatekniikka, Kotka
+    Insinööri (AMK) ohjelmistotekniikka, Mikkeli
+    Insinööri (AMK) peliohjelmointi, Kotka
+    Insinööri (AMK) rakennustekniikka, Kotka
+    Insinööri (AMK) rakennustekniikka, teollinen puurakentaminen, Savonlinna
+    Insinööri (AMK) robotiikka ja tekoäly, Kouvola, Kotka
+    Insinööri (AMK), sähkö- ja automaatiotekniikka, Mikkeli
+    Insinööri (AMK), talotekniikka, Mikkeli
+    Insinööri (AMK), ympäristöteknologia, Mikkeli
+    Jalkaterapeutti (AMK), jalkaterapeuttikoulutus, Savonlinna
+    Liikunnanohjaaja (AMK), Savonlinna
+    Merikapteeni (AMK), merenkulku, Kotka
+    Metsätalousinsinööri (AMK), metsätalous, Mikkeli
+    Muotoilija (AMK) biotuotemuotoilu, Kouvola
+    Muotoilija (AMK), graafinen muotoilu, Kouvola
+    Muotoilija (AMK), muoti ja puvustus, Kouvola
+    Muotoilija (AMK), palvelumuotoilu, Kouvola
+    Muotoilija (AMK), sisustusarkkitehtuuri ja kalustesuunnittelu, Kouvola
+    Naprapaatti (AMK), naprapaattikoulutus, Kotka
+    Restonomi (AMK), matkailu- ja palveluliiketoiminta, Mikkeli, Kouvola, verkko
+    Sairaanhoitaja (AMK), sairaanhoitajakoulutus, Kotka, Mikkeli ja Savonlinna
+    Sosionomi (AMK), sosionomikoulutus, Kotka ja Mikkeli
+    Sosionomi (AMK), kouluhyvinvointi ja kuraattorityö, Savonlinna
+    Terveydenhoitaja (AMK), terveydenhoitajakoulutus, Kouvola ja Mikkeli
+    Tradenomi (AMK), liiketalous, Kouvola, Mikkeli ja verkko
+    Tradenomi (AMK), data-analytiikka, Kouvola
+    Tradenomi (AMK), hyvinvointiliiketoiminnan johtaminen, Mikkeli
+    Tradenomi (AMK), liiketoiminnan logistiikka, Kotka
+    Tradenomi (AMK), myynti ja markkinointi, Kouvola
+    Tradenomi (AMK) tietojenkäsittely, verkko
+    Tradenomi (AMK), turvallisuusala, Savonlinna
+    Yhteisöpedagogi (AMK), yhteisöpedagogikoulutus, Mikkeli ja Kouvola
+
+Ylemmät AMK-tutkinnot
+Mikkelin kampus
+
+Xamkissa on tarjolla 31 ylempää AMK-tutkintoa, jotka toteutetaan XAMKin Master Schoolissa.
+
+    Akuutti hoitotyö, Kotka
+    Dataperustaisten hyvinvointipalvelujen kehittäminen, Mikkeli
+    Digimarkkinoinnin johtaminen, Mikkeli
+    Ensihoidon kehittäminen ja johtaminen, Kotka
+    Hyvinvoinnin edistäminen, Kouvola
+    International Business Management, Kouvola
+    Kestävä rakentaminen ja muotoilu, Kouvola
+    Kliininen asiantuntija, Mikkeli
+    Kulttuurihyvinvointi, Kouvola
+    Kyberturvallisuus, Kotka
+    Logistiikka, Kouvola
+    Matkailu- ja palveluliiketoiminta, Mikkeli
+    Merenkulun johtaminen, Kotka
+    Metsätalouden liiketoiminta, Mikkeli
+    Mielenterveyshoitotyö, Kotka
+    Monialainen toimintakyvyn edistäminen, Savonlinna
+    Monikulttuurisen asiantuntijuuden kehittäminen, Mikkeli
+    Naprapatia, Verkkototeutus, Kotka
+    Nuorisokasvatusten johtaminen ja kehittäminen, Mikkeli
+    Palvelumuotoilu, Kouvola
+    Projekti- ja myyntijohtaminen, Kotka, Mikkeli
+    rehabilitation, verkkototeutus
+    Sosiaali- ja terveysalan kehittäminen ja johtaminen, Mikkeli, Savonlinna
+    Sähkövoimatekniikka, Mikkeli
+    Talotekniikka, Mikkeli
+    Terveyden edistäminen, Kotka
+    Tiedonhallinta ja sähköinen arkistointi, Mikkeli
+    Tulevaisuuden liiketoiminnan johtaminen, Mikkeli
+    Tuloksellinen toimitusketjujen johtaminen, Kouvola
+    Ympäristöteknologia, Mikkeli
+    Yrittäjyys, Kouvola
+
+Avoin AMK ja täydennyskoulutus
+
+Xamk Pulsen avoimessa ammattikorkeakoulussa on yli 600 koulutusta vuosittain, ja opintoja järjestetään runsaasti verkossa.[4]
+
+Tarjonnassa on AMK- ja YAMK-tutkintojen opintojaksoja. Opetusta järjestetään tutkinto-opiskelijoiden tai avoimen AMK:n opiskelijoiden omissa ryhmissä. Opinnot ovat maksullisia, mutta myös maksuttomia opintojaksoja on tarjolla. Kaikille suunnattujen opintojen lisäksi avoimessa AMK:ssa on tarjolla jatkoväylä- ja polkuopintoja. Jatkoväyläopinnot ovat toisen asteen ammatillista tutkintoa opiskeleville ja polkuopinnot tutkintotavoitteisille opiskelijoille.
+
+Xamkin täydennyskoulutus tarjoaa maksullista koulutusta eri alojen ammattilaisille. Tarjolla on valmiita koulutuksia, jotka sisältävät verkko-opintoja ja joitakin päiviä lähiopetusta.
+Tutkimus-, kehittämis- ja innovaatiotoiminta
+
+Kaakkois-Suomen ammattikorkeakoulun tutkimus-, kehittämis- ja innovaatiotoiminnan (TKI) tavoitteena on erityisesti Etelä-Savon ja Kymenlaakson elinvoiman vahvistamiseen. Toiminnalla pyritään tukemaan yrityksiä ja luomaan uutta yrittäjyyttä. Vuosittain vireillä on noin 250 hanketta, joissa asiantuntijat etsivät, kokeilevat ja kehittävät uusia tuotteita ja palveluja tai tekevät tutkimusta yritysten ja työelämän tarpeisiin. Kansainvälinen TKI-toiminta keskittyy pääasiassa Eurooppaan, Itämeren alueelle sekä Venäjälle. TKI-toiminnan vahvuusalat ovat digitaalinen talous, kestävä hyvinvointi, logistiikka ja merenkulku sekä metsä, ympäristö ja energia. Xamkin 11 tutkimusyksikköä palvelevat erilaisissa tuotekehitys-, mittaus- ja analysointipalveluissa.[5]
+Kampukset
+Savonlinnan kampus
+
+Xamkin kampukset sijaitsevat Mikkelissä, Savonlinnassa, Kotkassa ja Kouvolassa. Kotkan kampuksella on noin 2 900 tekniikan, sosiaali- ja terveysalan opiskelijaa. Kouvolan kampuksella on noin 2 100 muotoilun, hyvinvointialojen, liiketalouden ja tekniikan opiskelijaa. Mikkelin kampuksella on reilut 4 000 insinööri-, metsätalousinsinööri-, restonomi- sekä hyvinvointialojen ja liiketalouden opiskelijaa. Savonlinnassa on noin 900 sosiaali-, terveys- ja liikunta-alan, tekniikan ja turvallisuusalan opiskelijaa.
+Omistajuus ja hallitus
+
+Xamk syntyi alun perin Kymenlaakson ammattikorkeakoulun ja Mikkelin ammattikorkeakoulun yhdistymisestä ja aloitti toimintansa korkeakouluna 1. tammikuuta 2017. Xamkin ylläpitäjänä toimii Kaakkois-Suomen Ammattikorkeakoulu Oy, jonka omistavat Mikkelin, Savonlinnan, Kotkan ja Kouvolan kaupungit.
+
+Xamkin hallitukseen kuuluu kahdeksan jäsentä. Hallituksen puheenjohtajana toimii Pekka Posti (15.10.2021 alkaen). Hallituksen muut jäsenet ovat:
+
+    Varapuheenjohtaja Kimmo Mikander, Mikkeli
+    Marjo Lakka, Kouvola
+    Pia Paukku, Kotka
+    Pirkko Rahikainen, Mikkeli
+    Kirsi Torikka, Savonlinna
+    Pekka Pulkkinen, henkilöstön keskuudestaan valitsema jäsen
+    Rico Martikainen, opiskelijoiden keskuudestaan valitsema jäsen """
